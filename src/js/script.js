80230724e1ce3280e341c0cd4d6e134a22f96474
Kelso <koasobrinho2008@gmail.com>
2026-03-31 10:06:19 -0300

    feat: Criação botão para cadastro de vinho

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-function botao(){
+function cadastro(){
 // cadastro cliente
 let nomeCliente = prompt("Digite o seu nome : ");
 let emailCliente = prompt("Digite o seu email: ");
@@ -6,11 +6,12 @@ alert("Cadastro realizado! Veja os detalhes no console.");
 
 console.log(`Nome de Usuário: ${nomeCliente}`)
 console.log(`Email de Usuário: ${emailCliente}`)
-
+}
+function vinho(){
 // informações do Vinho
-let nomeVinho = prompt("Digite o nome do vinho: ");
-let tipoVinho = prompt("Informe o Tipo do vinho: ");
-let safraVinho = parseInt(prompt("Informe a safra do vinho: "));
+let nomeVinho = prompt("Informe o nome do vinho: ");
+let tipoVinho = prompt("Informe o tipo vinho: ");
+let safraVinho = parseInt(prompt("Informe a safra vinho: "));
 let qtEstoqueVinho = parseInt(prompt("Informe o estoque do vinho: "));
 alert("As informações sobre o vinho estão disponíveis no console");
 
